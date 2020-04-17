e959f9e5d56057157f28bb1b4ab806cb1f7b93bb
tualli <tualli.shlee@gmail.com>
2020-04-18 03:38:54 +0900

    click시 배경색 바꾸기 구현완료

--- javascript.js
+++ javascript.js
@@ -1,13 +1,13 @@
 var ball = document.querySelector('#ball');
-var backgroundcolor = document;
+var presentcolor = document.body.style.background;
+var colorindicater = 0;
 var colorset = ['#d9598c', '#f1d2e7', '#bbb0dc', '#db706c', '#fcf695', '#a7e0e1', '#cee5d5', '#f2f2f2', '#b7d3e9', '#f1c3aa','#f3aa51','#567ace','url(./showcon.jpg) center/144%',];
 for(var i = 0; i<13; i++){
     document.querySelectorAll('#ball>div')[i].style.background = colorset[i];
     
 }
-document.querySelectorAll('div').forEach(function(element){
+document.querySelectorAll('#ball>div').forEach(function(element){
     element.addEventListener('click',function(){
-        console.log("안녕");    
-        //document.body.backgroundColor = element.backgroundColor;
+        document.body.style.background = element.style.background;
     });
 });
